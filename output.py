e5fda7deafd2b1ed99a71b7230846965ae9d6cb1
mlakota <miroslaw.lakota@gmail.com>
2010-07-30 12:10:42 +0200

    Add splitter class

--- output.py
+++ output.py
@@ -7,3 +7,16 @@ class fileWrite(object):
 
 	def close(self):
 		self.fileHandle.close()
+
+class splitter(object):
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+
+	def take(self, data):
+		self.a.take(data)
+		self.b.take(data)
+
+	def close(self):
+		self.a.close()
+		self.b.close()
