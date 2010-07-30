318512652972dd1a2ea1bd47cc902c8eff998559
mlakota <miroslaw.lakota@gmail.com>
2010-07-30 09:43:10 +0200

    Modified main.py:
    - Analyze parameters
    - Add function 'send()'

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 def main(lista):
-	print lista
+	if lista[0] == '--source-file':
+		source = lista[1]
+		if lista[2] == '--bufor':
+			bufor = lista[3]
+			if lista[4] == '--dest-file':
+				dest = lista[5]
+				send(source,bufor,dest)
+
+def send(source,bufor,dest):
+	pass
+
+
 
 if __name__ == '__main__':
 	main('--source-file ale --bufor 1000000 --dest-file kot'.split())
