325001fdf222dfbf02700c921d082403e52cf02b
mlakota <miroslaw.lakota@gmail.com>
2010-07-30 10:06:40 +0200

    Add send() body

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def main(lista):
 				send(source,bufor,dest)
 
 def send(source,bufor,dest):
-	pass
+	plik = open(source)
+	while 1:
+		temp = plik.read(bufor)
+		if not temp:
+			break
 
 
 
 if __name__ == '__main__':
-	main('--source-file ale --bufor 1000000 --dest-file kot'.split())
+	main('--source-file ala --bufor 1000000 --dest-file kot'.split())
