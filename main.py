6cd113e4093d23c13aff6d3c8f1410dd200999c4
mlakota <miroslaw.lakota@gmail.com>
2010-07-30 10:26:37 +0200

    Handle illegal parameter

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def main(lista):
 			if lista[4] == '--dest-file':
 				dest = lista[5]
 				send(source,bufor,dest)
-
+	else:
+		raise RuntimeError(lista[0])
 def send(source,bufor,dest):
 	plik = open(source,"rb")
 	plik2 = open(dest,"wb")
@@ -21,3 +22,4 @@ def send(source,bufor,dest):
 
 if __name__ == '__main__':
 	main('--source-file ala --bufor 1000000 --dest-file kot'.split())
+	main('--source-gen 10032410 --bufor 1000000 --dest-file kot'.split())
